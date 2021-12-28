a93c5f41ee90740087b9743778d317cc7758d4e3
Brian Takita <brian.takita@gmail.com>
2021-12-28 17:13:00 -0600

    lib instead of dist

--- bin/cache-instagram.mjs
+++ bin/cache-instagram.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { param_r_ } from '@ctx-core/cli-args'
-import { put_webdriver_scrape_cache } from '../dist/index.js'
+import { put_webdriver_scrape_cache } from '../lib/index.js'
 const { reload, help } = param_r_({
 	reload: '-r, --reload',
 	help: '-h, --help',
